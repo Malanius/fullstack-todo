22f4467e1cfa9d2601d3534834ab0e94fce4edd5
Malanius <malaniusprivierre@gmail.com>
2019-08-04 09:58:51 +0200

    Add basic increment

--- todo-fe/src/counter/Counter.jsx
+++ todo-fe/src/counter/Counter.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Counter.css';
 
-export default function Counter() {
-    return (
-        <div className="counter">
-            <button>+1</button>
-            <span className="count">0</span>
-        </div>
-    )
+export default class Counter extends Component {
+
+
+    state = {
+        counter: 0
+    }
+
+    increment = () => {
+        console.log('increment');
+        const oldCount = this.state.counter;
+        const newCount = oldCount + 1;
+        this.setState({ counter: newCount });
+    }
+
+    render() {
+        return (
+            <div className="counter" >
+                <button onClick={() => this.increment()}>+1</button>
+                <span className="count">{this.state.counter}</span>
+            </div>
+        )
+    }
 }
+
+
