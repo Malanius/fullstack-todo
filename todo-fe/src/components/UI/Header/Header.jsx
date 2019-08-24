fba75cb41deebf201cac68fe52eba397132da409
Malanius <malaniusprivierre@gmail.com>
2019-08-24 19:16:34 +0200

    Hide nav links for non-logged user

--- todo-fe/src/components/UI/Header/Header.jsx
+++ todo-fe/src/components/UI/Header/Header.jsx
@@ -1,22 +1,34 @@
 import React from 'react';
+import { withRouter} from 'react-router';
 import { NavLink } from 'react-router-dom';
+import Auth from '../../../Auth/Auth';
 
 
-export default () => {
+const header = () => {
+    
+    const isUserLogged = Auth.isLoggedIn();
+    var user = '';
+    if(isUserLogged){
+        user = Auth.getUser();
+    }
+
+    const homeLink = isUserLogged ? <li><NavLink className="nav-link" to={`/welcome/${user}`}>Home</NavLink></li> : null;
+
     return (
         <header>
             <nav className="navbar navbar-expand-md navbar-dark bg-dark">
                 <a href="https://github.com/malanius" className="navbar-brand">Malanius</a>
                 <ul className="navbar-nav">
-                    {/* TODO: remove hardocded username from here */}
-                    <li><NavLink className="nav-link" to="/welcome/test">Home</NavLink></li>
-                    <li><NavLink className="nav-link" to="/todos">TODOs</NavLink></li>
+                    {homeLink}                    
+                    {isUserLogged && <li><NavLink className="nav-link" to="/todos">TODOs</NavLink></li>}
                 </ul>
                 <ul className="navbar-nav navbar-collapse justify-content-end">
-                    <li><NavLink className="nav-link" to="/login">Login</NavLink></li>
-                    <li><NavLink className="nav-link" to="/logout">Logout</NavLink></li>
+                    {!isUserLogged && <li><NavLink className="nav-link" to="/login">Login</NavLink></li>}
+                    {isUserLogged &&<li><NavLink className="nav-link" to="/logout" onClick={() => Auth.deregisterLogin()}>Logout</NavLink></li>}
                 </ul>
             </nav>
         </header>
     )
 }
+
+export default withRouter(header);
