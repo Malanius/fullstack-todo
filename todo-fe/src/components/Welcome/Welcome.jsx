fd7fbf9bf2aca48cf1b9d4f607799e86f63aab00
Malanius <malaniusprivierre@gmail.com>
2019-08-31 15:08:08 +0200

    Prepare welcome pace for communication with BE

--- todo-fe/src/components/Welcome/Welcome.jsx
+++ todo-fe/src/components/Welcome/Welcome.jsx
@@ -2,12 +2,21 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default (props) => {
+
+    const retrieveWelcomeMessage = () =>{
+        console.log('retrieve message')
+    }
+
     return (
         <>
             <h1>Welcome</h1>
             <div className="container">
                 Welcome to the TODO app, {props.match.params.name}!
                 <p><Link to="/todos">View TODOs</Link></p>
+            </div>
+            <div className="container">
+                Click here to get customized message.
+                <button onClick={retrieveWelcomeMessage} className="btn btn-success">Get welcome message</button>
             </div>
         </>
     )
