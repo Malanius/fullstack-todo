16e8e7e3fe5a747aa880fc3f43bdadd846f0ae56
Malanius <malaniusprivierre@gmail.com>
2019-09-01 11:01:54 +0200

    Implement delete todo on FE

--- todo-fe/src/containers/TodoList/TodoList.jsx
+++ todo-fe/src/containers/TodoList/TodoList.jsx
@@ -6,18 +6,32 @@ import TodoDataService from './TodoDataService';
 export class TodoList extends Component {
 
     state = {
-        todos: []
+        todos: [],
+        message: ''
     }
 
-    componentDidMount(){
+    componentDidMount() {
+        this.fetchTodos();
+    }
+
+    fetchTodos() {
         TodoDataService.retrieveAllTodos(Auth.getUser())
-        .then(response => this.setState({todos: response.data}))
+            .then(response => this.setState({ todos: response.data }))
+    }
+
+    deleteTodo(id) {
+        TodoDataService.deleteTodo(Auth.getUser(), id)
+            .then(response => {
+                this.setState({ message: `Delete of todo ${id} sucessfull` });
+                this.fetchTodos();
+            })
     }
 
     render() {
         return (
             <div className="todoList">
                 <h1>List of TODOs</h1>
+                {this.state.message && <div className="alert alert-success">{this.state.message}</div>}
                 <div className="container">
                     <table className="table">
                         <thead>
@@ -25,6 +39,7 @@ export class TodoList extends Component {
                                 <th>Description</th>
                                 <th>Done?</th>
                                 <th>Due date</th>
+                                <th>Delete</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -33,6 +48,9 @@ export class TodoList extends Component {
                                     <td>{todo.description}</td>
                                     <td>{todo.done ? 'Yes' : 'No'}</td>
                                     <td>{todo.deadline}</td>
+                                    <td><button className="btn btn-danger"
+                                        onClick={() => this.deleteTodo(todo.id)}>Delete</button>
+                                    </td>
                                 </tr>
                             ))}
                         </tbody>
