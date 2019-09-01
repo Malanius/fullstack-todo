0c32b1f36d0101dde249c8dfd3e72c067e978b11
Malanius <malaniusprivierre@gmail.com>
2019-09-01 11:33:26 +0200

    Prepare todo edit page

--- todo-fe/src/components/Todo/Todo.jsx
+++ todo-fe/src/components/Todo/Todo.jsx
@@ -1,10 +1,38 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Formik, Form, Field } from 'formik';
+import moment from 'moment';
 
 export default class Todo extends Component {
+
+    state = {
+        id: this.props.match.params.id,
+        description: 'Todo 1',
+        deadline: moment(new Date()).format('YYYY-MM-DD')
+    }
+
     render() {
         return (
             <div>
-                Todo component for id - {this.props.match.params.id}
+                <h1>Todo</h1>
+                <div className="container">
+                    <Formik>
+                        {
+                            (props) => (
+                                <Form>
+                                    <fieldset className="form-group">
+                                        <label>Description</label>
+                                        <Field className="form-control" type="text" name="description"/>
+                                    </fieldset>
+                                    <fieldset className="form-group">
+                                        <label>Deadline</label>
+                                        <Field className="form-control"type="date" name="deadline"/>
+                                    </fieldset>
+                                    <button className="btn btn-success" type="submit">Save</button>
+                                </Form>
+                            )
+                        }
+                    </Formik>
+                </div>
             </div>
         )
     }
