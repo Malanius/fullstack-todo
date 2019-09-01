1b1d7c3fd9c5f8c13612a751764ebc02a0f2c6ef
Malanius <malaniusprivierre@gmail.com>
2019-09-01 14:09:31 +0200

    Create endpoint for new todo creation on BE

--- todo-be/src/main/java/net/malanius/todo/rest/TodoController.java
+++ todo-be/src/main/java/net/malanius/todo/rest/TodoController.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -43,5 +45,17 @@ public class TodoController {
     public ResponseEntity<Todo> updateTodo(@PathVariable String username, @PathVariable long id, @RequestBody Todo todo) {
         Todo todoUpdated = todoService.saveTodo(todo);
         return ResponseEntity.ok(todoUpdated);
+    }
+
+    @PostMapping("/users/{username}/todos")
+    public ResponseEntity<Void> createTodo(@PathVariable String username, @RequestBody Todo todo) {
+        todo.setId(-1);
+        Todo todoCreated = todoService.saveTodo(todo);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(todoCreated.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
     }
 }
