24c93ae92b4622085ab09fdbec93a89804ee2b98
Malanius <malaniusprivierre@gmail.com>
2019-09-03 18:35:19 +0200

    Prepare authentication endpoint for login on BE

---
+++ todo-be/src/main/java/net/malanius/todo/rest/AuthenticationBean.java
@@ -0,0 +1,12 @@
+package net.malanius.todo.rest;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+class AuthenticationBean {
+
+    private String message;
+
+}
