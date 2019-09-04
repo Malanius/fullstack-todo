8ed5e878742fa6ec201ef4a8dc154beb71bad2da
Malanius <malaniusprivierre@gmail.com>
2019-09-04 18:05:37 +0200

    Convert Todo to entity

--- todo-be/src/main/java/net/malanius/todo/data/Todo.java
+++ todo-be/src/main/java/net/malanius/todo/data/Todo.java
@@ -3,15 +3,24 @@ package net.malanius.todo.data;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.util.Date;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
+@Entity
 public class Todo {
 
-    private long id;
+    @Id
+    @GeneratedValue
+    private Long id;
+
     private String username;
     private String description;
     private Date deadline;
---
+++ todo-be/src/main/resources/data.sql
@@ -0,0 +1,14 @@
+insert into todo(id, username, description, deadline, completed)
+values (10001, 'test', 'Todo 1', sysdate(), false );
+
+insert into todo(id, username, description, deadline, completed)
+values (10002, 'test', 'Todo 2', sysdate(), false );
+
+insert into todo(id, username, description, deadline, completed)
+values (10003, 'test', 'Todo 3', sysdate(), false );
+
+insert into todo(id, username, description, deadline, completed)
+values (10004, 'test', 'Todo 4', sysdate(), false );
+
+insert into todo(id, username, description, deadline, completed)
+values (10005, 'test', 'Todo 5', sysdate(), false );
