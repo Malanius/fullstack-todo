1fc4aeeba2fbe0d6eb80dc09a378891aa89dc246
Malanius <malaniusprivierre@gmail.com>
2019-09-04 18:16:24 +0200

    Use todo repository instead of hardcoded service

--- todo-be/src/main/java/net/malanius/todo/rest/TodoController.java
+++ todo-be/src/main/java/net/malanius/todo/rest/TodoController.java
@@ -1,8 +1,8 @@
 package net.malanius.todo.rest;
 
 import net.malanius.todo.data.Todo;
+import net.malanius.todo.data.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -14,43 +14,41 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:3000")
 public class TodoController {
 
-    private final TodoHardcodedService todoService;
+    private final TodoRepository todoRepository;
 
     @Autowired
-    public TodoController(TodoHardcodedService todoService) {
-        this.todoService = todoService;
+    public TodoController(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
     }
 
     @GetMapping("/users/{username}/todos")
     public List<Todo> getAllTodos(@PathVariable String username) {
-        return todoService.findAll();
+        return todoRepository.findByUsername(username);
     }
 
     @GetMapping("/users/{username}/todos/{id}")
     public Todo getTodoById(@PathVariable String username, @PathVariable long id) {
-        return todoService.findById(id);
+        return todoRepository.findById(id).get();
     }
 
     @DeleteMapping("/users/{username}/todos/{id}")
     public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable long id) {
-        Todo todo = todoService.deleteById(id);
-        if (todo != null) {
-            return ResponseEntity.noContent().build();
-        }
-
-        return ResponseEntity.notFound().build();
+        todoRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 
     @PutMapping("/users/{username}/todos/{id}")
     public ResponseEntity<Todo> updateTodo(@PathVariable String username, @PathVariable long id, @RequestBody Todo todo) {
-        Todo todoUpdated = todoService.saveTodo(todo);
+        todo.setUsername(username);
+        Todo todoUpdated = todoRepository.save(todo);
         return ResponseEntity.ok(todoUpdated);
     }
 
     @PostMapping("/users/{username}/todos")
     public ResponseEntity<Void> createTodo(@PathVariable String username, @RequestBody Todo todo) {
-        todo.setId(-1);
-        Todo todoCreated = todoService.saveTodo(todo);
+
+        todo.setUsername(username);
+        Todo todoCreated = todoRepository.save(todo);
         URI location = ServletUriComponentsBuilder
                 .fromCurrentRequest()
                 .path("/{id}")
